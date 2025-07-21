ca66b33edd6a297415465d33add9a5e8c8ea2c6a
rezwanahammad <127617637+rezwanahammad@users.noreply.github.com>
2025-07-22 01:22:53 +0600

    Add project files.

---
+++ My PF/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using My_PF.Models;
+
+namespace My_PF.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Education()
+        {
+            return View();
+        }
+
+        public IActionResult Skills()
+        {
+            return View();
+        }
+        public IActionResult Projects()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
